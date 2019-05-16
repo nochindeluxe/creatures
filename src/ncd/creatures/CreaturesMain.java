b803ac8b6b3438dfe614255a16ece01f0529a2f2
David <David@DESKTOP-FHN7HQ8.attlocal.net>
2019-05-16 14:16:46 -0400

    init commit

---
+++ src/ncd/creatures/CreaturesMain.java
@@ -0,0 +1,22 @@
+
+package ncd.creatures;
+
+import processing.core.PApplet;
+
+public class CreaturesMain extends PApplet {
+    
+    @Override 
+    public void settings() {
+        size(1280, 720);
+    }
+    
+    @Override
+    public void setup() {
+        
+    }
+    
+    @Override 
+    public void draw() {
+        background(255);
+    }
+}
---
+++ src/ncd/creatures/Main.java
@@ -0,0 +1,9 @@
+package ncd.creatures;
+
+public class Main {
+
+    public static void main(String[] args) {
+       CreaturesMain.main("ncd.creatures.CreaturesMain");
+    }
+    
+}
